534d32b00bc9d64fd58c298c907b559b6cb0ea2c
XLIORF <xliorf@163.com>
2023-07-01 23:53:11 +0800

    app

--- app/oled_disp.c
+++ app/oled_disp.c
@@ -3,20 +3,13 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
-struct reg_access_transimission
-{
-    char * buffer;
-    unsigned int len;
-}reg_access_transimission;
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-
-    reg_access_transimission.len = 1;
-    reg_access_transimission.buffer = malloc(reg_access_transimission.len);
+    char str[] = "     Hello         milk-v duo!";
     int fd = open("/dev/oled_128_64_1", O_RDWR);
-    ioctl(fd,_IOW('o', 1, struct reg_access_transimission),&reg_access_transimission);
-    free(reg_access_transimission.buffer);
+    write(fd,str,strlen(str));
+    close(fd);
     return 0;
 }
